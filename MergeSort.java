8a1e0218c106f8fdb9ae690dd20a73bbb0676b44
kadapana <168061066+kadapana@users.noreply.github.com>
2025-06-17 14:59:03 +0530

    Update MergeSort.java

--- MergeSort.java
+++ MergeSort.java
@@ -1,29 +1,54 @@
+import java.util.Scanner;
+
 public class MergeSort {
     public static void main(String[] args) {
-        int[] arr = {64, 25, 12, 22, 11};  
-
-        System.out.println("Original Array:");
-        printArray(arr);
-
-        mergeSort(arr, 0, arr.length - 1);
-
-        System.out.println("\nSorted Array in Ascending Order:");
-        printArray(arr);
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        mergeSort(arr, 0, n - 1);
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
     }
 
-  
-    public static void mergeSort(int[] arr, int left, int right) {
+    static void mergeSort(int[] arr, int left, int right) {
         if (left < right) {
-          
             int mid = (left + right) / 2;
-
-          
             mergeSort(arr, left, mid);
             mergeSort(arr, mid + 1, right);
-
-        
             merge(arr, left, mid, right);
         }
     }
+
+    static void merge(int[] arr, int left, int mid, int right) {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+        for (int i = 0; i < n1; i++) {
+            L[i] = arr[left + i];
+        }
+        for (int j = 0; j < n2; j++) {
+            R[j] = arr[mid + 1 + j];
+        }
+        int i = 0, j = 0, k = left;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k++] = L[i++];
+            } else {
+                arr[k++] = R[j++];
+            }
+        }
+        while (i < n1) {
+            arr[k++] = L[i++];
+        }
+        while (j < n2) {
+            arr[k++] = R[j++];
+        }
+    }
+}
 
     
