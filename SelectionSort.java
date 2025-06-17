ea5adc233c51cb5603014300ebe717529d5f8a36
kadapana <168061066+kadapana@users.noreply.github.com>
2025-06-17 14:57:43 +0530

    Update SelectionSort.java

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,43 +1,27 @@
+
+import java.util.Scanner;
+
 public class SelectionSort {
     public static void main(String[] args) {
-        int[] arr = {64, 25, 12, 22, 11};  
-
-        System.out.println("Original Array:");
-        printArray(arr);
-
-        selectionSort(arr);
-
-        System.out.println("\nSorted Array in Ascending Order:");
-        printArray(arr);
-    }
-
-    
-    public static void selectionSort(int[] arr) {
-        int n = arr.length;
-
-       
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
         for (int i = 0; i < n - 1; i++) {
-            
             int minIndex = i;
-
             for (int j = i + 1; j < n; j++) {
                 if (arr[j] < arr[minIndex]) {
                     minIndex = j;
                 }
             }
-
-           
             int temp = arr[minIndex];
             arr[minIndex] = arr[i];
             arr[i] = temp;
         }
-    }
-
-  
-    public static void printArray(int[] arr) {
-        for (int value : arr) {
-            System.out.print(value + " ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
         }
-        System.out.println();
     }
 }
