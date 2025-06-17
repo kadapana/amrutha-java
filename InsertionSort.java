63b693c46de0dc4dcebb81074216006f9c715241
kadapana <168061066+kadapana@users.noreply.github.com>
2025-06-17 15:00:55 +0530

    Update InsertionSort.java

--- InsertionSort.java
+++ InsertionSort.java
@@ -1,39 +1,25 @@
- public class InsertionSort {
+import java.util.Scanner;
+
+public class InsertionSort {
     public static void main(String[] args) {
-        int[] arr = {64, 25, 12, 22, 11};  
-
-        System.out.println("Original Array:");
-        printArray(arr);
-
-        insertionSort(arr);
-
-        System.out.println("\nSorted Array in Ascending Order:");
-        printArray(arr);
-    }
-
-  
-    public static void insertionSort(int[] arr) {
-        int n = arr.length;
-
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
         for (int i = 1; i < n; i++) {
             int key = arr[i];
             int j = i - 1;
-
-        
             while (j >= 0 && arr[j] > key) {
                 arr[j + 1] = arr[j];
                 j--;
             }
-
             arr[j + 1] = key;
         }
-    }
-
-  
-    public static void printArray(int[] arr) {
-        for (int value : arr) {
-            System.out.print(value + " ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
         }
-        System.out.println();
     }
 }
+
